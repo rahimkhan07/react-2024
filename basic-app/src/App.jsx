104ea2b6285d070fc66f087096516f5e2aa2eec3
rahimkhan07 <140167765+rahimkhan07@users.noreply.github.com>
2025-07-06 03:28:48 +0530

    add in repo

---
+++ basic-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./Chai.jsx"
+
+function App() {
+
+  return (
+    <>
+    {/* <h1>Hashbunallahu wa-nemal wakeel</h1> */}
+    <Chai />
+    </>
+    
+  )
+}
+
+export default App
